8a18aa99d35dc27a791c8d5866c4cecc7b6b5c8c
adkshishir <98789290+adkshishir@users.noreply.github.com>
2024-04-10 11:30:44 +0545

    Init
     Back up backend things to do

--- app/Http/Controllers/Api/MenuController.php
+++ app/Http/Controllers/Api/MenuController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+
+use function Laravel\Prompts\select;
 
 class MenuController extends Controller
 {
     public function index(){
-        $posts=Post::select('id','title','slug','image','author_id','created_at')->with(['category',function($query){
-            $query->select('id','name','slug');
+        $posts =Category::select('id','name','slug')->with(['posts'=>function($query){
+            $query->select('title','slug','author_id','image');
         }])->get();
         if($posts){
             return response()->json([
                 'success'=>true,
                 'status'=>200,
-                'posts'=>$posts
+                'data'=>$posts
             ],200);
         }
         else{
